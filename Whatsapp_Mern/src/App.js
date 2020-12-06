d0a4370e57ce7e7a1cfc146d1b884b35c1e6cf5d
Neha Kalbande <neha.leenaharsh@gmail.com>
2020-12-06 23:49:44 +0530

    testing

---
+++ Whatsapp_Mern/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("cc9e57e6e2d34b5acc99", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessages) => {
+      // alert(JSON.stringify(newMessages));
+      setMessages([...messages, newMessages]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat messages={messages} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Whatsapp_Mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWAuk2wdYl31lunuiN3eAlpRebYBk9XuE",
+  authDomain: "whatsapp-mern-a4f01.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-a4f01.firebaseio.com",
+  projectId: "whatsapp-mern-a4f01",
+  storageBucket: "whatsapp-mern-a4f01.appspot.com",
+  messagingSenderId: "25492696736",
+  appId: "1:25492696736:web:3ef36ea20f000f9b1923c5",
+};
